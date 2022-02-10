232791ca245f573267901fa0ccf5ba250bfe2405
CundullahT-CT <cterzioglu@cybertekschool.com>
2022-02-10 20:52:36 +0100

    fixes XMLReader Class

--- src/com/cydeo/solid/dependencyInversion/example/good/XMLReader.java
+++ src/com/cydeo/solid/dependencyInversion/example/good/XMLReader.java
@@ -2,6 +2,7 @@ package com.cydeo.solid.dependencyInversion.example.good;
 
 public class XMLReader implements Reader {
 
+    @Override
     public String getUsername() {
         return "<username>mikesmith</username>";
     }
