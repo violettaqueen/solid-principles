1fbf469f4c0679a067e72ec5e805dc4c02911e53
violettaqueen <violetta.v.queen@gmail.com>
2022-08-30 10:37:18 -0600

    Initial commit

---
+++ src/com/cydeo/solid/singleResponsibility/example/good/Manager.java
@@ -0,0 +1,4 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class Manager {
+}
---
+++ src/com/cydeo/solid/singleResponsibility/example/good/UserController.java
@@ -0,0 +1,16 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class UserController {
+
+    public static void getRequest(){
+        // get request
+        // send this request to validation and security part
+
+    }
+    public static String createResponse(){
+        return "Response"; // for ex. User is created
+    }
+    public static void sendResponse(){
+
+    }
+}
